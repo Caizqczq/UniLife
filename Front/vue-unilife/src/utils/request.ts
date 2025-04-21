b772fb5069271b7771731c6fcbddb95c906522a4
abab2320 <1589841436@qq.com>
2025-04-21 08:49:18 +0800

    添加Token

--- Front/vue-unilife/src/utils/request.ts
+++ Front/vue-unilife/src/utils/request.ts
@@ -5,10 +5,17 @@ const service = axios.create({
   timeout: 5000
 });
 
-service.interceptors.request.use(
+service.interceptors.request.use( 
   config => {
+    const token = localStorage.getItem('token');
+    if (token) {
     console.log("前端发送信息");
     return config;
+    }
+    else 
+    { console.log("没有token");
+      return config;
+    }
   },
   error => {
     // 对请求错误做些什么
